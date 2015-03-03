baadc1e5d39e20f0beb79c48f4602bf558c2b56f
kty <kty0427@naver.com>
2015-03-03 11:54:14 +0900

    메인 네비게이션 작업중1

--- index.php
+++ index.php
@@ -19,11 +19,12 @@
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	<script src="vendors/jquery.easings.min.js"></script>
 	<script type="text/javascript" src="./js/jquery.fullPage.min.js"></script>
+	<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 	<script type="text/javascript">
 		$(document).ready(function() {
 			$('#fullpage').fullpage({
-				sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-				anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
+				sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', '#ccddff'],
+				anchors: ['firstPage', 'secondPage', '3rdPage', 'lastPage'],
 				menu: '#menu',
 				css3: true,
 				scrollingSpeed: 1000
@@ -40,53 +41,6 @@
 			});
 		});
 	</script>
-	<script>
-      // 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script');
-
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: 'M7lc1UVf-VE',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
-	  ytplayer = document.getElementById("movie_player");
-	  ytplayer.getCurrentTime()=2;
-	  alert(ytplayer);
-
-    </script>
-
 </head>
 <body>
 <div id="fullpage">
@@ -98,6 +52,47 @@
 	    
 	</div>
 </div>
+<div id="infoMenu">
+	<ul>
+		<li><a href="https://github.com/alvarotrigo/fullPage.js/archive/master.zip">Download</a></li>
+		<li>
+			<a href="#" id="showExamples">Examples</a>
+			<div id="examplesList">
+				<div class="column">
+					<h3>Navigation</h3>
+					<ul>
+					<li><a href="examples/scrollBar.html">Scroll Bar Enabled</a></li>
+					<li><a href="examples/normalScroll.html">Normal scrolling</a></li>
+					<li><a href="examples/continuous.html">Continuous vertical</a></li>
+					<li><a href="examples/noAnchor.html">Without anchor links (same URL)</a></li>
+					<li><a href="examples/navigationV.html">Vertical navigation dots</a></li>
+					<li><a href="examples/navigationH.html">Horizontal navigation dots</a></li>
+					</ul>
+				</div>
+				<div class="column">
+					<h3>Design</h3>
+					<ul>
+						<li><a href="examples/backgrounds.html">Full backgrounds</a></li>
+						<li><a href="examples/videoBackground.html">Full background videos</a></li>
+						<li><a href="examples/gradientBackgrounds.html">Gradient backgrounds</a></li>
+						<li><a href="examples/scrolling.html">Scrolling inside sections and slides</a></li>
+						<li><a href="examples/fixedHeaders.html">Adding fixed header and footer</a></li>
+						<li><a href="examples/oneSection.html">One single section</a></li>
+					</ul>
+				</div>
+				<div class="column">
+					<h3>Other</h3>
+					<ul>
+						<li><a href="examples/apple.html">Animations on scrolling</a></li>
+						<li><a href="examples/methods.html">Functions and methods</a></li>
+					</ul>
+				</div>
+			</div>
 
+		</li>
+		<li><a href="https://github.com/alvarotrigo/fullPage.js#fullpagejs">Documentation</a></li>
+		<li><a href="http://alvarotrigo.com/#contact-page">Contact</a></li>
+	</ul>
+</div>
 </body>
 </html>
