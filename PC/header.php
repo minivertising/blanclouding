eb4c511eb16e938c870c6545a28437e698859ebb
kyhfan <yh.kim@minivertising.kr>
2015-03-03 17:56:17 +0900

    메타태그 추가2

--- PC/header.php
+++ PC/header.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta property="og:title" content="더페이스샵-블랑클라우닝">
     <meta property="og:type" content="website" />
-    <meta property="og:url" content="http://ohuimall.co.kr/index_1.php">
+    <meta property="og:url" content="http://www.thefaceshopclouding.co.kr/PC/index.php">
     <meta property="og:image" content="http://www.ohuimall.co.kr/images/can_img_1.jpg" />
     <meta property="og:description" content="어릴 적 당신이 지녔던 '그 마음'을 팝니다">
     <title>Document</title>
