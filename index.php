997e46864e9e2a984b5c9caee246a7d43eb8be1a
kyhfan <yh.kim@minivertising.kr>
2015-03-03 11:52:46 +0900

    시스템 구조 변경

--- index.php
+++ index.php
@@ -1,103 +1,20 @@
-<?php
+<?
+	include_once "config.php";
+
+	//unset($media);
+	//$media		= $_REQUEST['media'];
+	//$goods_idx	= $_REQUEST['goods_idx'];
+
+	//$_SESSION['ss_media'] = $media;
+
+	OM_InsertTrackingInfo($media, $gubun);
+	if($check_mobile)
+	{
+		Header("Location:http://www.thefaceshopclouding.co.kr/MOBILE/index.php");
+		exit;
+	}else{
+		Header("Location:http://www.thefaceshopclouding.co.kr/PC/index.php");
+		exit;
+	}
 
 ?>
-
-<!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml">
-
-<head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>fullPage.js One Page Scroll Site Plugin</title>
-	<meta name="author" content="Alvaro Trigo Lopez" />
-	<meta name="description" content="fullPage plugin by Alvaro Trigo. Create fullscreen pages fast and simple. One page scroll jquery plugin like iPhone 5 website." />
-	<meta name="keywords"  content="fullpage,jquery,alvaro,trigo,plugin,fullscren,screen,full,iphone5,apple" />
-	<meta name="Resource-type" content="Document" />
-	<link rel="stylesheet" type="text/css" href="./css/jquery.fullPage.css" />
-	<link rel="stylesheet" type="text/css" href="example3.css" />
-
-
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-	<script src="vendors/jquery.easings.min.js"></script>
-	<script type="text/javascript" src="./js/jquery.fullPage.min.js"></script>
-	<script type="text/javascript">
-		$(document).ready(function() {
-			$('#fullpage').fullpage({
-				sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke', '#ccddff'],
-				anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
-				menu: '#menu',
-				css3: true,
-				scrollingSpeed: 1000
-			});
-
-			$('#showExamples').click(function(e){
-				e.stopPropagation();
-				e.preventDefault();
-				$('#examplesList').toggle();
-			});
-
-			$('html').click(function(){
-				$('#examplesList').hide();
-			});
-		});
-	</script>
-	<script>
-      // 2. This code loads the IFrame Player API code asynchronously.
-      var tag = document.createElement('script');
-
-      tag.src = "https://www.youtube.com/iframe_api";
-      var firstScriptTag = document.getElementsByTagName('script')[0];
-      firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-      // 3. This function creates an <iframe> (and YouTube player)
-      //    after the API code downloads.
-      var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          height: '390',
-          width: '640',
-          videoId: 'M7lc1UVf-VE',
-          events: {
-            'onReady': onPlayerReady,
-            'onStateChange': onPlayerStateChange
-          }
-        });
-      }
-
-      // 4. The API will call this function when the video player is ready.
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
-
-      // 5. The API calls this function when the player's state changes.
-      //    The function indicates that when playing a video (state=1),
-      //    the player should play for six seconds and then stop.
-      var done = false;
-      function onPlayerStateChange(event) {
-        if (event.data == YT.PlayerState.PLAYING && !done) {
-          setTimeout(stopVideo, 6000);
-          done = true;
-        }
-      }
-      function stopVideo() {
-        player.stopVideo();
-      }
-	  ytplayer = document.getElementById("movie_player");
-	  ytplayer.getCurrentTime()=2;
-	  alert(ytplayer);
-
-    </script>
-
-</head>
-<body>
-<div id="fullpage">
-
-	<div class="section active" id="section0">
-	<iframe width="1280" height="720" src="https://www.youtube.com/embed/tnbG4iB_Rc4?rel=0&amp;controls=0&amp;showinfo=0" frameborder="0" allowfullscreen></iframe>
-		</div>
-	<div class="section" id="section1">
-	    
-	</div>
-</div>
-
-</body>
-</html>
