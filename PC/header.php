7c37250e1d8bae120fd57ab58b10f28e1a42d5e1
kyhfan <yh.kim@minivertising.kr>
2015-03-04 15:13:59 +0900

    페이스북 문구 수정2

--- PC/header.php
+++ PC/header.php
@@ -8,7 +8,7 @@
     <meta property="og:title" content="더페이스샵-블랑클라우닝">
     <meta property="og:type" content="website" />
     <meta property="og:url" content="http://goo.gl/6RUiAK">
-    <meta property="og:image" content="http://www.ohuimall.co.kr/images/can_img_1.jpg" />
+    <!-- <meta property="og:image" content="http://www.ohuimall.co.kr/images/can_img_1.jpg" /> -->
     <meta property="og:description" content="1. 서장훈, 촉촉하게 수지랑! 서장훈 구름탄 기분이랄까~촉촉한 선물 2. 서장훈 더페이스샵 CF모델? '아니아니 그게 아니고' 공개! 구름선물">
     <title>Document</title>
     <link rel="stylesheet" type="text/css" href="../lib/fullpage/jquery.fullPage.css" />
