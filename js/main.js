e1a3528b50186f5a4e5d594d4b54e5815d5e8982
kty <kty0427@naver.com>
2015-03-06 17:36:59 +0900

    병합

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
-
-  window.fbAsyncInit = function() {
-    FB.init({
-      appId      : '376515979199685',
-      xfbml      : true,
-      version    : 'v2.2'
-    });
-  };
-
-  (function(d, s, id){
-     var js, fjs = d.getElementsByTagName(s)[0];
-     if (d.getElementById(id)) {return;}
-     js = d.createElement(s); js.id = id;
-     js.src = "//connect.facebook.net/en_US/sdk.js";
-     fjs.parentNode.insertBefore(js, fjs);
-   }(document, 'script', 'facebook-jssdk'));
-
 function addr_change(addr1)
 {
 	$.ajax({
