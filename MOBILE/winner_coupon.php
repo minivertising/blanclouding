100d3bd65f770cfba914fb3b166090c0cb98215c
kty <kty0427@naver.com>
2015-03-09 10:24:26 +0900

    모바일 페이지작업
    시간 변환.

--- MOBILE/winner_coupon.php
+++ MOBILE/winner_coupon.php
@@ -48,8 +48,33 @@ print_r($user_info['shop_addr']);
     {
         $("#map_div").hide();
     }
+	
+	<!--- hide 
+	var millenium = new Date("March 10, 2015 22:00:00") //이곳을 수정하면 됩니다
+	function CalcRemaining(theForm)
+	{
+	var now = new Date();
 
+	var difference = parseInt(((millenium.getTime() - now.getTime()) / 1000) + 0.999)
+	var secs = difference % 60
+
+	difference = parseInt(difference / 60)
+	var minutes  = difference % 60
+
+	difference = parseInt(difference / 60)
+	var hours  = difference % 1000000
+
+	difference = parseInt(difference / 24)
+	var days  = difference
+
+	theForm.txtHours.value = hours;
+	theForm.txtMins.value = minutes;
+	theForm.txtSecs.value = secs;
+
+	setTimeout("CalcRemaining(document.clock)", 250);
+	}
     </script>
+	
 	<?
 	/*
 		$lg_zip =  "SELECT * FROM ".$_gl['member_info_table']." WHERE";
@@ -68,8 +93,22 @@ print_r($user_info['shop_addr']);
 		
 
 	?>
-  <body>
-    남은 시간 : <?=$hour.":".$minute?>
+  <body onLoad=CalcRemaining(document.clock) onLoad="timerONE=window.setTimeout('scrollit_r21(50)',100);">
+	<h4><B><font color="maroon">이벤트 참여 남은시간</B></h4></font>
+	<TABLE WIDTH="250" >
+	<FORM NAME=clock>
+		<TD COLSPAN=4>
+		<INPUT TYPE=TEXT NAME=txtHours SIZE=4 onFocus="blur()"><BR>Hours
+		<TD>
+		<INPUT TYPE=TEXT NAME=txtMins SIZE=4 onFocus="blur()"><BR>Minutes 
+		<TD>
+		<INPUT TYPE=TEXT NAME=txtSecs SIZE=4 onFocus="blur()"><BR>Seconds
+		<tr align=center>
+
+	</FORM>
+	</TABLE>
+
+	</FORM>
     <input type="button" value="지도" onclick="show_map()">
     <div id="map_area" style="width:50%; heigh:50%">
     </div>
