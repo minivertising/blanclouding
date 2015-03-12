7002433908e4838b0126285aca909b7600f36b83
kyhfan <yh.kim@minivertising.kr>
2015-03-12 17:05:28 +0900

    당첨 페이지 세팅

--- MOBILE/winner_coupon.php
+++ MOBILE/winner_coupon.php
@@ -44,7 +44,7 @@
         $("#map_div").hide();
       }
 	
-		var millenium = new Date("March 11, 2015 22:00:00") //이곳을 수정하면 됩니다
+		var millenium = new Date("March 15, 2015 22:00:00") //이곳을 수정하면 됩니다
 
 		function CalcRemaining(theForm)
 		{
