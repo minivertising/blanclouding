5000e47f954762bb5b9f6803dd2b5291ab3a4446
Seonhye yang <yangseonhye@yangseonhyeui-iMac.local>
2015-03-12 18:47:01 +0900

    페북 og 태그 수정

--- PC/header.php
+++ PC/header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />
-    <meta property="og:title" content="더페이스샵-블랑클라우닝" />
+    <meta property="og:title" content="더페이스샵 블란클라우딩" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="http://www.thefaceshopclouding.co.kr/?media=fb" />
     <!-- <meta property="og:image" content="http://www.ohuimall.co.kr/images/can_img_1.jpg" /> -->
