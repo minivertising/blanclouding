d28692b0fab9e260a6862d2911bd9b8c593eae5f
kyhfan <yh.kim@minivertising.kr>
2015-03-13 19:50:56 +0900

    페이스북 메세지 수정

--- MOBILE/header.php
+++ MOBILE/header.php
@@ -8,11 +8,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />
-    <meta property="og:title" content="더페이스샵 블란클라우딩" />
+    <meta property="og:title" content="서장훈이 화장품 CF를?!" />
     <meta property="og:type" content="website" />
     <meta property="og:url" content="http://www.thefaceshopclouding.co.kr/MOBILE/index.php" />
     <meta property="og:image" content="http://www.thefaceshopclouding.co.kr/PC/images/sns_image.jpg" />
-    <meta property="og:description" content="1. 서장훈, 촉촉하게 수지랑! 서장훈 구름탄 기분이랄까~촉촉한 선물 2. 서장훈 더페이스샵 CF모델? '아니아니 그게 아니고' 공개! 구름선물" />
+    <meta property="og:description" content="< 아니 아니, 그게 아니고~ > 전격 공개! 건조한 피부에 봄비같은 하얀 수분 크림 출시! 지금 10ml Kit도 받아가세요!    더페이스샵 - 블란클라우딩" />
     <link rel="shortcut icon" type="image/x-icon" href="./images/icon/favicon.ico" />
     <title>THEFACESHOP</title>
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
