d7ebf33fdb6d32b7ee9e2bc968417f7c2be7cae4
kyhfan <yh.kim@minivertising.kr>
2015-03-16 14:10:06 +0900

    short url2

--- admin/short_url.php
+++ admin/short_url.php
@@ -4,7 +4,7 @@
 	$long_url = "www.naver.com";
 	$bitly_url = "http://api.bit.ly/v3/shorten";
 	$request_url = $bitly_url . "?login=" . $bitly_login . "&apiKey=" . $bitly_api_key . "&longUrl=" . urlencode($long_url);
-	$url = "https://api-ssl.bitly.com/v3/shorten?access_token=2b19a46fe91ec689eac5abf76352584176df2650&longUrl=http://www.naver.com&format=json";
+	//$url = "https://api-ssl.bitly.com/v3/shorten?access_token=2b19a46fe91ec689eac5abf76352584176df2650&longUrl=http://www.naver.com&format=json";
 	$response = file_get_contents_curl($request_url);
 	$response = json_decode($response);
 	$short_url = $response->data->url;
