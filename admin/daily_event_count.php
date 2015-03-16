fd3799d159534d829dca33b88b1d3ab53b192161
kyhfan <yh.kim@minivertising.kr>
2015-03-16 20:04:08 +0900

    일자별 이벤트 참여자2

--- admin/daily_event_count.php
+++ admin/daily_event_count.php
@@ -35,9 +35,9 @@
 	
 	while ($date_data = @mysqli_fetch_array($res))
 	{		
-		$pc_query		= "SELECT * FROM ".$_gl['tracking_info_table']." WHERE tracking_date LIKE  '%".$date_data['mb_date']."%' AND tracking_gubun='PC'";
+		$pc_query		= "SELECT * FROM ".$_gl['member_info_table']." WHERE mb_regdate LIKE  '%".$date_data['mb_date']."%' AND mb_gubun='PC'";
 		$pc_count		= mysqli_num_rows(mysqli_query($my_db, $pc_query));
-		$mobile_query	= "SELECT * FROM ".$_gl['tracking_info_table']." WHERE tracking_date LIKE  '%".$date_data['mb_date']."%' AND tracking_gubun='MOBILE'";
+		$mobile_query	= "SELECT * FROM ".$_gl['member_info_table']." WHERE mb_regdate LIKE  '%".$date_data['mb_date']."%' AND mb_gubun='MOBILE'";
 		$mobile_count	= mysqli_num_rows(mysqli_query($my_db, $mobile_query));
 		$total_count = $pc_count + $mobile_count;
 
