67470811e28dfa3ddaa2e1f5098f74954b11378a
kyhfan <yh.kim@minivertising.kr>
2015-03-16 13:47:01 +0900

    모바일 쿠폰 날짜 변경

--- MOBILE/winner_coupon.php
+++ MOBILE/winner_coupon.php
@@ -44,7 +44,7 @@
       }
 	
 
-		var millenium = new Date("March 15, 2015 22:00:00") //이곳을 수정하면 됩니다
+		var millenium = new Date("March 19, 2015 22:00:00") //이곳을 수정하면 됩니다
 
 		function CalcRemaining(theForm)
 		{
---
+++ admin/short_url.php
@@ -0,0 +1,21 @@
+<?
+	$bitly_login = "kyhfan@gmail.com";
+	$bitly_api_key = "2b19a46fe91ec689eac5abf76352584176df2650";
+	$long_url = "www.naver.com";
+	$bitly_url = "http://api.bit.ly/v3/shorten";
+	$request_url = $bitly_url . "?login=" . $bitly_login . "&apiKey=" . $bitly_api_key . "&longUrl=" . urlencode($long_url);
+	$response = file_get_contents_curl($request_url);
+	$response = json_decode($response);
+	$short_url = $response->data->url;
+
+print_r($short_url);
+	function file_get_contents_curl($url) {
+		$ch = curl_init();
+		curl_setopt($ch, CURLOPT_HEADER, 0);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_URL, $url); 
+		$data = curl_exec($ch);
+		curl_close($ch); 
+		return $data;
+	}
+?>
