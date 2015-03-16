7e16159bb59dbdd8b7595c14966705d5a1ddca5e
kyhfan <yh.kim@minivertising.kr>
2015-03-16 14:43:32 +0900

    short_url7

--- admin/short_url.php
+++ admin/short_url.php
@@ -1,23 +1,33 @@
 <?
-	$bitly_login = "kyhfan";
-	$bitly_api_key = "R_11ea80ffc2bf4bbe8c848b761e71df8a";
-	$long_url = "www.naver.com";
-	$bitly_url = "http://api.bit.ly/v3/shorten";
-	$request_url = $bitly_url . "?login=" . $bitly_login . "&apiKey=" . $bitly_api_key . "&longUrl=" . urlencode($long_url);
-	//$url = "https://api-ssl.bitly.com/v3/shorten?access_token=2b19a46fe91ec689eac5abf76352584176df2650&longUrl=http://www.naver.com&format=json";
-	$response = file_get_contents_curl($request_url);
-	$response = json_decode($response);
-	$short_url = $response->data->url;
+/* returns the shortened url */
+function get_bitly_short_url($url,$login,$appkey,$format='txt') {
+	$connectURL = 'http://api.bit.ly/v3/shorten?login='.$login.'&apiKey='.$appkey.'&uri='.urlencode($url).'&format='.$format;
+	return curl_get_result($connectURL);
+}
 
-print_r($short_url);
-	function file_get_contents_curl($url) {
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_HEADER, 0);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_URL, $url); 
-		$data = curl_exec($ch);
-		curl_close($ch); 
-		return $data;
-	}
+/* returns expanded url */
+function get_bitly_long_url($url,$login,$appkey,$format='txt') {
+	$connectURL = 'http://api.bit.ly/v3/expand?login='.$login.'&apiKey='.$appkey.'&shortUrl='.urlencode($url).'&format='.$format;
+	return curl_get_result($connectURL);
+}
+
+/* returns a result form url */
+function curl_get_result($url) {
+	$ch = curl_init();
+	$timeout = 5;
+	curl_setopt($ch,CURLOPT_URL,$url);
+	curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+	curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+	$data = curl_exec($ch);
+	curl_close($ch);
+	return $data;
+}
+
+/* get the short url */
+$short_url = get_bitly_short_url('http://www.naver.com','kyhfan','R_11ea80ffc2bf4bbe8c848b761e71df8a');
+
+/* get the long url from the short one */
+$long_url = get_bitly_long_url($short_url,'kyhfan','R_11ea80ffc2bf4bbe8c848b761e71df8a');
+
 ?>
 
