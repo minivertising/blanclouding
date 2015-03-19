ef7d1bda87c84f0a4978a1806080711e3970397a
kyhfan <yh.kim@minivertising.kr>
2015-03-19 20:04:10 +0900

    페이스북 영상 공유2

--- PC/header_movie1.php
+++ PC/header_movie1.php
@@ -12,15 +12,20 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0" />
 
-<meta property="og:site_name" content="Staging Derbywire">
-<meta property="og:url" content="http://staging.derbywire.com/video/ticker/102">
-<meta property="og:title" content="Derbywire mobile market place">
+<meta property="og:site_name" content="ther">
+<meta property="og:url" content="http://www.thefaceshopclouding.co.kr/PC/movie1.php">
+<meta property="og:title" content="서장훈이 화장품 CF를?!">
 <meta property="og:type" content="video">
-<meta property="og:image" content="http://s3.amazonaws.com/derbywire_development/system/attachments/96/original/open-uri20130822-8464-10q2etf?1377170817">
-<meta property="og:description" content="Derbywire is a collective space where creatives can sell buy digital content">
+<meta property="og:image" content="http://www.thefaceshopclouding.co.kr/PC/images/sns_image.jpg">
+<meta property="og:description" content="< 아니 아니, 그게 아니고~ > 전격 공개! 건조한 피부에 봄비같은 하얀 수분 크림 출시! 지금 10ml Kit도 신청하세요!    더페이스샵 - 블란클라우딩">
 <meta property="og:video" content="https://youtu.be/XDpe5Trw-zs">
 <meta property="og:video:type" content="application/x-shockwave-flash">
 <meta property="og:video:width" content="480">
+    <!-- <meta property="og:title" content="서장훈이 화장품 CF를?!" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="http://www.thefaceshopclouding.co.kr/PC/movie1.php" />
+    <meta property="og:video" content="https://youtu.be/XDpe5Trw-zs" />
+    <meta property="og:description" content="< 아니 아니, 그게 아니고~ > 전격 공개! 건조한 피부에 봄비같은 하얀 수분 크림 출시! 지금 10ml Kit도 신청하세요!    더페이스샵 - 블란클라우딩" /> -->
     <link rel="shortcut icon" type="image/x-icon" href="./images/pavicon.ico" />
     <title>THEFACESHOP</title>
     <link rel="stylesheet" type="text/css" href="./css/style.css" />
