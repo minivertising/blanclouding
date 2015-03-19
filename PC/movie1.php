95cce6a17e7ef359ac45d9ceb547f3bb565f3a10
kyhfan <yh.kim@minivertising.kr>
2015-03-19 20:07:41 +0900

    페이스북 영상 공유3

--- PC/movie1.php
+++ PC/movie1.php
@@ -1,6 +1,6 @@
 <?
 	include_once "./header_movie1.php";
 ?>
-111
+<iframe allowfullscreen="1" src="<?=$_gl['youtube_url']?>" frameborder="0" width="450px" name="ytplayer1" id="ytplayer1" ></iframe>
 </body>
 </html>
