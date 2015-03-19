8ac51650f786415b753c1e4b8c4448eb3f275e34
kyhfan <yh.kim@minivertising.kr>
2015-03-19 19:20:28 +0900

    분기 수정

--- index.php
+++ index.php
@@ -6,17 +6,22 @@
 	$pg		= $_REQUEST['pg'];
 
 	$_SESSION['ss_media'] = $media;
-
+$pg
+exit;
 	BC_InsertTrackingInfo($media, $gubun);
 	if($gubun == "MOBILE")
 	{
-		if ($pg)
+		if ($pg == "movie1")
 			Header("Location:http://www.thefaceshopclouding.co.kr/MOBILE/movie1.php");
+		else if ($pg == "movie2")
+			Header("Location:http://www.thefaceshopclouding.co.kr/MOBILE/movie2.php");
 		else
 			Header("Location:http://www.thefaceshopclouding.co.kr/MOBILE/index.php");
 		exit;
 	}else{
-		if ($pg)
+		if ($pg == "movie1")
+			Header("Location:http://www.thefaceshopclouding.co.kr/PC/movie1.php");
+		else if ($pg == "movie2")
 			Header("Location:http://www.thefaceshopclouding.co.kr/PC/movie2.php");
 		else
 			Header("Location:http://www.thefaceshopclouding.co.kr/PC/index.php");
