9cab88d5ec498ec90b14e73fbbc0475496921969
kyhfan <yh.kim@minivertising.kr>
2015-03-20 18:31:34 +0900

    영상 수정

--- include/global.php
+++ include/global.php
@@ -6,7 +6,7 @@
 	$_gl['share_info_table']			= "share_info";             //공유정보 테이블
 	$_gl['sms_info_table']			= "sms_info";             //문자정보 테이블
 
-	$_gl['youtube_url']				= "https://www.youtube.com/embed/XDpe5Trw-zs?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=1&rel=0&autoplay=1"; // 유튜브 URL
+	$_gl['youtube_url']				= "https://www.youtube.com/embed/1kRP0yqnA9o?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=1&rel=0&autoplay=1"; // 유튜브 URL
 	$_gl['youtube_url1']				= "https://www.youtube.com/embed/XDpe5Trw-zs?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=1"; // 유튜브 URL 1
 	$_gl['youtube_url2']				= "https://www.youtube.com/embed/XDpe5Trw-zs?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=1"; // 유튜브 URL 2
 	$_gl['youtube_url3']				= "https://www.youtube.com/embed/XDpe5Trw-zs?controls=0&loop=1&modestbranding=1&showinfo=0&wmode=opaque&enablejsapi=1"; // 유튜브 URL 3
