27f0e88fcfb494f78bc647da6193953149f7e360
kyhfan <yh.kim@minivertising.kr>
2015-03-23 17:04:49 +0900

    모바일 수정4

--- js/main.js
+++ js/main.js
@@ -535,7 +535,7 @@ function movie_share(media, num)
 			}
 		});
 	}else if (media == "kt"){
-		Kakao.init('62027fc7fd5be42191c4c2e4787386ca');
+		//Kakao.init('62027fc7fd5be42191c4c2e4787386ca');
 
 		// 카카오톡 링크 버튼을 생성합니다. 처음 한번만 호출하면 됩니다.
 		Kakao.Link.createTalkLinkButton({
